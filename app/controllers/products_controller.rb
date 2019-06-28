0218b0f14191d871b1a0992a6718cbf7602065ba
Nicolas Marini <nmarini44@gmail.com>
2019-06-28 21:45:10 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,6 +27,7 @@ class ProductsController < ApplicationController
     respond_to do |format|
       format.html { render :show }
       format.json { render json: @product }
+      binding.pry
     end
   end
 
